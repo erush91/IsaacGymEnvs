a11f66340ba41dce6aedab4a6ef542af15386952
Gene Rush <erush91@gmail.com>
2022-09-25 18:07:32 -0600

    Added hexapod task. Just copied Ant tasks. Tomorrow, I will add the 5th and 6th legs!

--- isaacgymenvs/tasks/__init__.py
+++ isaacgymenvs/tasks/__init__.py
@@ -39,6 +39,7 @@ from .factory.factory_task_nut_bolt_place import FactoryTaskNutBoltPlace
 from .factory.factory_task_nut_bolt_screw import FactoryTaskNutBoltScrew
 from .franka_cabinet import FrankaCabinet
 from .franka_cube_stack import FrankaCubeStack
+from .hexapod import Hexapod
 from .humanoid import Humanoid
 from .humanoid_amp import HumanoidAMP
 from .ingenuity import Ingenuity
@@ -55,6 +56,7 @@ isaacgym_task_map = {
     "AnymalTerrain": AnymalTerrain,
     "BallBalance": BallBalance,
     "Cartpole": Cartpole,
+    "Hexapod": Hexapod,
     "FactoryTaskGears": FactoryTaskGears,
     "FactoryTaskInsertion": FactoryTaskInsertion,
     "FactoryTaskNutBoltPick": FactoryTaskNutBoltPick,
